9c0ff354d861d5e716d025c2a8abb968fe9a57d6
vishnucode17 <61362200+vishnucode17@users.noreply.github.com>
2021-06-21 12:29:02 +0530

    Update settings.py

--- ecom/settings.py
+++ ecom/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 TEMPLATES_DIR=os.path.join(BASE_DIR,'templates')
@@ -141,3 +143,5 @@ STATICFILES_DIRS=[
     ]
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+django_heroku.settings(locals())
